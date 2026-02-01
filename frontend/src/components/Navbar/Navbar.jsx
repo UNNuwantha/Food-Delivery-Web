e692914b8b743122fc4d94edd5df6f2ff8be5b70
UNNuwantha <www.nadun50@gmail.com>
2026-02-01 06:46:25 +0530

    Creating Components

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="Logo" />
+    </div>
+  )
+}
+
+export default Navbar
