bf38d3d31240da087520a8bda6cf87c40d81ad1e
UNNuwantha <www.nadun50@gmail.com>
2026-03-14 14:48:04 +0530

    Created User Authentications

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
+import userRouter from './routes/userRoute.js'
+
 
 dotenv.config()
 
@@ -22,10 +24,19 @@ const startServer = async () => {
 
         //api endpoints
         app.use("/api/food", foodRouter)
-
+        app.use("/api/user", userRouter)
+        
         app.get("/", (req, res) => {
             res.send("API Working")
         })
+
+        // Error handling middleware
+        app.use((err, req, res, next) => {
+            if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+                return res.status(400).json({ success: false, message: 'Invalid JSON format' });
+            }
+            next(err);
+        });
 
         app.listen(port, () => {
             console.log(`Server Started on http://localhost:${port}`)
