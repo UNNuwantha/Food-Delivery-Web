c21e0455eceeddc8b3ec2706998e4f5473b71f0c
UNNuwantha <www.nadun50@gmail.com>
2026-03-14 14:47:25 +0530

    Created User Authentications

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import React, { useContext, useState } from 'react'
 import'./Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
 
    const[menu,setMenu]=useState("menu");
+   const [showProfileDropdown, setShowProfileDropdown] = useState(false);
 
-   const{getTotalCartAmount} =useContext(StoreContext);
+   const{getTotalCartAmount,token,setToken} =useContext(StoreContext);
+   
+   const Navigate = useNavigate();
 
+   const logout = () => {
+       localStorage.removeItem("token");
+       setToken("");
+        Navigate("/");
+   }
+   
   return (
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="Logo" className="logo" /></Link>
@@ -25,7 +34,17 @@ const Navbar = ({setShowLogin}) => {
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
+        {!token?
         <button onClick={()=>setShowLogin(true)}>sign in</button>
+        :<div className='navbar-profile' onMouseEnter={() => setShowProfileDropdown(true)} onMouseLeave={() => setShowProfileDropdown(false)}>
+          <img src={assets.profile_icon} alt="" />
+          <ul className={showProfileDropdown ? "navbar-profile-dropdown show" : "navbar-profile-dropdown"}>
+            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr />
+            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>          
+          </div>}
+        
       </div>
     </div>
   )
